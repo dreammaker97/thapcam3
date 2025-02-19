96e50623e900d660f9159fde66f27751a43bff85
dreammaker97 <87482298+dreammaker97@users.noreply.github.com>
2025-02-19 17:58:02 +0700

    Update userScript.js

--- mods/userScript.js
+++ mods/userScript.js
@@ -1,8 +1,1 @@
-import 'whatwg-fetch';
-import './domrect-polyfill';
-import './adblock.js';
-import './sponsorblock.js';
-import './ui.js';
-import './speedUI.js';
-import './theme.js';
-import './modernUI.js';
+
